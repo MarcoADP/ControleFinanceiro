db1fbaa2cd64aedeb25cccc343b6e1d461512599
Marco Aurelio Deoldoto Paulino <marcoadpaulino@gmail.com>
2023-02-01 15:15:02 -0300

    Criar Spring Cloud Gateway para roteamento dos serviços

---
+++ Gateway/src/main/java/com/github/marcoadp/gateway/GatewayApplication.java
@@ -0,0 +1,31 @@
+package com.github.marcoadp.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+
+    @Bean
+    public RouteLocator routes(RouteLocatorBuilder builder) {
+        return builder
+                .routes()
+                /* TODO ADICIONAR ROTAS
+                .route(r -> r.path("/acesso/**").uri("lb://msacesso"))
+                .route(r -> r.path("/bancario/**").uri("lb://msbancario"))
+                .route(r -> r.path("/financeiro/**").uri("lb://msfinanceiro"))
+                .route(r -> r.path("/investimento/**").uri("lb://msinvestimento"))
+                 */
+                .build();
+    }
+
+}
